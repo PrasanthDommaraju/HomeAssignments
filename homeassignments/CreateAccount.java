8a56d5128ed0778ff615a4ca09dd0fc9a3e4f455
PrasanthDommaraju <prasanth.dommaraju@gmail.com>
2024-06-26 14:47:19 +0530

    Update CreateAccount.java

--- homeassignments/CreateAccount.java
+++ homeassignments/CreateAccount.java
@@ -15,58 +15,80 @@ public class CreateAccount {
 		
 		ca.manage().window().maximize();
 		
+		//login to leaftaps		
 		ca.findElement(By.id("username")).sendKeys("demosalesmanager");
 		ca.findElement(By.id("password")).sendKeys("crmsfa");
 		ca.findElement(By.className("decorativeSubmit")).click();
 		
+		//click on CRMSFA
 		ca.findElement(By.partialLinkText("CRM")).click();
 		
+		//click on Accounts tab and select Create Account		
 		ca.findElement(By.xpath("//a[text()='Accounts']")).click();
 		ca.findElement(By.xpath("//a[text()='Create Account']")).click();
 		
+		//getting page title for Create Account page
+		String createAccountTitle = ca.getTitle();
+		System.out.println("Create Account Page Title is "+createAccountTitle);
+		
 		//ca.findElement(By.xpath("//input[@id='accountName']"));
 		
+		//entering Account name
 		WebElement accNameEntered = ca.findElement(By.xpath("//input[@id='accountName']"));
 		
-		accNameEntered.sendKeys("PrashantK");
+		accNameEntered.sendKeys("PrasantK");
 		
 		String accNameValue = accNameEntered.getAttribute("value");
 		
 		System.out.println(accNameValue);
-				
+		
+		//selecting industry from dropdown
 		WebElement industryWE = ca.findElement(By.xpath("//select[@name='industryEnumId']"));
 		Select industryDD = new Select(industryWE);
 		industryDD.selectByIndex(3);
 		
+		//selecting ownership from dropdown
 		WebElement ownershipWE = ca.findElement(By.xpath("//select[@name='ownershipEnumId']"));
 		Select ownershipDD = new Select(ownershipWE);
 		ownershipDD.selectByVisibleText("S-Corporation");
 		
+		//selecting source from dropdown
 		WebElement sourceWE = ca.findElement(By.xpath("(//select[@class='inputBox'])[4]"));
 		Select sourceDD = new Select(sourceWE);
 		sourceDD.selectByValue("LEAD_EMPLOYEE");
 		
+		//selecting campaign from drop down
 		WebElement mCampaignWE = ca.findElement(By.xpath("(//select[@class='inputBox'])[5]"));
 		Select mCampaignDD = new Select(mCampaignWE);
 		mCampaignDD.selectByIndex(6);
 		
+		//selecting state from dropdown
 		WebElement stateWE = ca.findElement(By.xpath("(//select[@class='selectBox'])[2]"));
 		Select stateDD = new Select(stateWE);
 		stateDD.selectByValue("TX");
 		
+		//entering description
 		ca.findElement(By.xpath("//textarea[contains(@name,'description')]")).sendKeys("Selenium Automation Tester");
 		
 		Thread.sleep(3000);
 		
+		//creating Account by clicking on Create Account
 		ca.findElement(By.xpath("//input[@class='smallSubmit']")).click();
+		
+		//getting title of page after account creation
+		String accCreateTitle = ca.getTitle();
+		
+		System.out.println("View Account Page Title is "+accCreateTitle);
 		
 		Thread.sleep(3000);
 		
+		//get text for Account Name
 		WebElement accName = ca.findElement(By.xpath("(//span[@class='tabletext'])[3]"));
 		String text = accName.getText();
 		
 		System.out.println(text);
 		
+		//verifying if account name is displayed correctly or not
 		if (text.contains(accNameValue)) {
 			
 			System.out.println("Account Name displayed correctly");
